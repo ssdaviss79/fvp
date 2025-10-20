e6c04a58b1bdb41b277331e857af798cb02393f1
ssdaviss79 <daviss79@yahoo.com>
2025-10-20 21:02:32 +0800

    Update FvpPlugin.h

--- darwin/Classes/FvpPlugin.h
+++ darwin/Classes/FvpPlugin.h
@@ -1,10 +1,4 @@
 #import <Flutter/Flutter.h>
 #import <AVFoundation/AVFoundation.h>
-
 @interface FvpPlugin : NSObject<FlutterPlugin>
-
-// Picture-in-Picture layer registration
-+ (void)registerPipLayer:(CALayer *)layer forTextureId:(int64_t)textureId;
-+ (void)unregisterPipLayerForTextureId:(int64_t)textureId;
-
 @end
