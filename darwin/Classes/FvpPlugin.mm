6935ec41a89996d15e6ed788097f4988e1b231b5
ssdaviss79 <daviss79@yahoo.com>
2025-10-20 16:08:16 +0800

    Update FvpPlugin.mm5

--- darwin/Classes/FvpPlugin.mm
+++ darwin/Classes/FvpPlugin.mm
@@ -18,7 +18,7 @@ using namespace std;
 @interface MetalTexture : NSObject<FlutterTexture>
 @end
 @implementation MetalTexture {
-@public
+    @public
     id<MTLDevice> device;
     id<MTLCommandQueue> cmdQueue;
     id<MTLTexture> texture;
@@ -33,37 +33,28 @@ using namespace std;
     if (self) {
         device = MTLCreateSystemDefaultDevice();
         cmdQueue = [device newCommandQueue];
-        MTLTextureDescriptor *td =
-            [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:MTLPixelFormatBGRA8Unorm
-                                                               width:width
-                                                              height:height
-                                                           mipmapped:NO];
+        auto td = [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:MTLPixelFormatBGRA8Unorm width:width height:height mipmapped:NO];
         td.usage = MTLTextureUsageRenderTarget;
         texture = [device newTextureWithDescriptor:td];
-
-        auto attr = CFDictionaryCreateMutable(kCFAllocatorDefault, 0,
-                                              &kCFTypeDictionaryKeyCallBacks,
-                                              &kCFTypeDictionaryValueCallBacks);
+        //assert(!texture.iosurface); // CVPixelBufferCreateWithIOSurface(fltex.iosurface)
+        auto attr = CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
         CFDictionarySetValue(attr, kCVPixelBufferMetalCompatibilityKey, kCFBooleanTrue);
-        auto iosurface_props =
-            CFDictionaryCreateMutable(kCFAllocatorDefault, 0,
-                                      &kCFTypeDictionaryKeyCallBacks,
-                                      &kCFTypeDictionaryValueCallBacks);
-        CFDictionarySetValue(attr, kCVPixelBufferIOSurfacePropertiesKey, iosurface_props);
+        auto iosurface_props = CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+        CFDictionarySetValue(attr, kCVPixelBufferIOSurfacePropertiesKey, iosurface_props); // optional?
         CVPixelBufferCreate(nil, width, height, kCVPixelFormatType_32BGRA, attr, &pixbuf);
         CFRelease(attr);
         texCache = nullptr;
 #if (USE_TEXCACHE + 0)
         CVMetalTextureCacheCreate(nullptr, nullptr, device, nullptr, &texCache);
         CVMetalTextureRef cvtex;
-        CVMetalTextureCacheCreateTextureFromImage(nil, texCache, pixbuf, nil,
-                                                  MTLPixelFormatBGRA8Unorm,
-                                                  width, height, 0, &cvtex);
+        CVMetalTextureCacheCreateTextureFromImage(nil, texCache, pixbuf, nil, MTLPixelFormatBGRA8Unorm, width, height, 0, &cvtex);
         fltex = CVMetalTextureGetTexture(cvtex);
         CFRelease(cvtex);
 #else
         auto iosurface = CVPixelBufferGetIOSurface(pixbuf);
-        td.usage = MTLTextureUsageShaderRead;
+        td.usage = MTLTextureUsageShaderRead; // Unknown?
+// macos: failed assertion `Texture Descriptor Validation IOSurface textures must use MTLStorageModeManaged or MTLStorageModeShared'
+// ios: failed assertion `Texture Descriptor Validation IOSurface textures must use MTLStorageModeShared
         fltex = [device newTextureWithDescriptor:td iosurface:iosurface plane:0];
 #endif
     }
@@ -78,24 +69,17 @@ using namespace std;
     }
 }
 - (CVPixelBufferRef _Nullable)copyPixelBuffer {
+    //return CVPixelBufferRetain(pixbuf);
     std::lock_guard<std::mutex> lock(mtx);
-    id<MTLCommandBuffer> cmdbuf = [cmdQueue commandBuffer];
-    id<MTLBlitCommandEncoder> blit = [cmdbuf blitCommandEncoder];
-    [blit copyFromTexture:texture
-              sourceSlice:0
-              sourceLevel:0
-             sourceOrigin:MTLOriginMake(0, 0, 0)
-               sourceSize:MTLSizeMake(texture.width, texture.height, texture.depth)
-               toTexture:fltex
-        destinationSlice:0
-        destinationLevel:0
-       destinationOrigin:MTLOriginMake(0, 0, 0)];
+    auto cmdbuf = [cmdQueue commandBuffer];
+    auto blit = [cmdbuf blitCommandEncoder];
+    [blit copyFromTexture:texture sourceSlice:0 sourceLevel:0 sourceOrigin:MTLOriginMake(0, 0, 0) sourceSize:MTLSizeMake(texture.width, texture.height, texture.depth)
+        toTexture:fltex destinationSlice:0 destinationLevel:0 destinationOrigin:MTLOriginMake(0, 0, 0)]; // macos 10.15
     [blit endEncoding];
     [cmdbuf commit];
     return CVPixelBufferRetain(pixbuf);
 }
 @end
-
 
 // NEW: AVSampleBufferDisplayLayer for PiP (hidden, frame-fed from FFmpeg)
 @interface PipDisplayLayer : NSObject
@@ -109,8 +93,8 @@ using namespace std;
         _textureId = textureId;
         _displayLayer = [AVSampleBufferDisplayLayer layer];
         _displayLayer.videoGravity = AVLayerVideoGravityResizeAspect;
-        _displayLayer.hidden = YES;
-        _displayLayer.frame = CGRectZero;
+        _displayLayer.hidden = YES;  // Hidden for PiP only
+        _displayLayer.frame = CGRectZero;  // Offscreen
     }
     return self;
 }
@@ -121,44 +105,30 @@ using namespace std;
 }
 @end
 
-
-class TexturePlayer final : public Player
+class TexturePlayer final: public Player
 {
 public:
-    TexturePlayer(int64_t handle, int width, int height,
-                  NSObject<FlutterTextureRegistry>* texReg)
+    TexturePlayer(int64_t handle, int width, int height, NSObject<FlutterTextureRegistry>* texReg)
         : Player(reinterpret_cast<mdkPlayerAPI*>(handle))
     {
         mtex_ = [[MetalTexture alloc] initWithWidth:width height:height];
         texId_ = [texReg registerTexture:mtex_];
-
         MetalRenderAPI ra{};
-        ra.device   = (__bridge void*)mtex_->device;
+        ra.device = (__bridge void*)mtex_->device;
         ra.cmdQueue = (__bridge void*)mtex_->cmdQueue;
-        ra.texture  = (__bridge void*)mtex_->texture;
+        ra.texture = (__bridge void*)mtex_->texture;
         setRenderAPI(&ra);
         setVideoSurfaceSize(width, height);
-
-        // SAFELY call Objective-C from within C++ lambda
-        setRenderCallback([this, texReg](void* opaque) {
+        setRenderCallback([this, texReg](void* opaque){
             std::lock_guard<std::mutex> lock(mtex_->mtx);
             renderVideo();
-
-            int64_t tid = this->texId_;
-            __unsafe_unretained NSObject<FlutterTextureRegistry>* registry = texReg;
-
-            // Notify Flutter on main thread
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [registry textureFrameAvailable:tid];
-            });
-
-            // Bridge to PiP
+            [texReg textureFrameAvailable:texId_];
+            
+            // NEW: Bridge to AVSampleBufferDisplayLayer for PiP
             bridgeFrameToPipLayer();
         });
-
-        pipLayer = nil;
+        pipLayer = nil;  // Initialize
     }
-
     ~TexturePlayer() override {
         setRenderCallback(nullptr);
         setVideoSurfaceSize(-1, -1);
@@ -167,43 +137,36 @@ public:
             pipLayer = nil;
         }
     }
-
     int64_t textureId() const { return texId_; }
-
+    
+    // NEW: Bridge FFmpeg frame to AVSampleBufferDisplayLayer
     void bridgeFrameToPipLayer() {
         if (!pipLayer || !pipLayer.displayLayer) return;
-        CVPixelBufferRef pixbuf = mtex_->pixbuf;
+        
+        CVPixelBufferRef pixbuf = mtex_->pixbuf;  // From FFmpeg decode
         if (!pixbuf) return;
-
-        CMSampleTimingInfo timing = {
-            .presentationTimeStamp = kCMTimeInvalid,
-            .duration = kCMTimeInvalid,
-            .decodeTimeStamp = kCMTimeInvalid
-        };
-
+        
+        // Create CMSampleBuffer from pixbuf (timing from FFmpeg PTS)
+        CMSampleTimingInfo timing = { .presentationTimeStamp = kCMTimeInvalid, .duration = kCMTimeInvalid, .decodeTimeStamp = kCMTimeInvalid };  // Get from FFmpeg (e.g., frame->pts)
+        // Assume getFrameTiming() from mdk (adapt as needed)
         CMVideoFormatDescriptionRef formatDesc;
-        OSStatus status = CMVideoFormatDescriptionCreateForImageBuffer(kCFAllocatorDefault,
-                                                                       pixbuf,
-                                                                       &formatDesc);
+        OSStatus status = CMVideoFormatDescriptionCreateForImageBuffer(kCFAllocatorDefault, pixbuf, &formatDesc);
         if (status != noErr) return;
-
+        
         CMSampleBufferRef sampleBuffer;
-        status = CMSampleBufferCreateReadyWithImageBuffer(kCFAllocatorDefault,
-                                                          pixbuf, formatDesc,
-                                                          &timing, &sampleBuffer);
+        status = CMSampleBufferCreateReadyWithImageBuffer(kCFAllocatorDefault, pixbuf, formatDesc, &timing, &sampleBuffer);
         CFRelease(formatDesc);
         if (status != noErr || !sampleBuffer) return;
-
+        
         [pipLayer.displayLayer enqueueSampleBuffer:sampleBuffer];
         CFRelease(sampleBuffer);
     }
-
+    
 private:
     int64_t texId_ = 0;
     MetalTexture* mtex_ = nil;
-    PipDisplayLayer *pipLayer = nil;
+    PipDisplayLayer *pipLayer = nil;  // NEW: For PiP
 };
-
 
 @interface FvpPlugin () {
     std::unordered_map<int64_t, std::shared_ptr<TexturePlayer>> players;
@@ -213,17 +176,14 @@ private:
 
 @implementation FvpPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-    id<FlutterBinaryMessenger> messenger = [registrar messenger];
 #if TARGET_OS_OSX
-    // macOS: do not alter audio session
+    auto messenger = registrar.messenger;
 #else
+    auto messenger = [registrar messenger];
     // Allow audio playback when the Ring/Silent switch is set to silent
     [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
 #endif
-
-    FlutterMethodChannel* channel =
-        [FlutterMethodChannel methodChannelWithName:@"fvp"
-                                    binaryMessenger:messenger];
+    FlutterMethodChannel* channel = [FlutterMethodChannel methodChannelWithName:@"fvp" binaryMessenger:messenger];
     FvpPlugin* instance = [[FvpPlugin alloc] initWithRegistrar:registrar];
 #if TARGET_OS_OSX
 #else
@@ -231,8 +191,4 @@ private:
 #endif
     [registrar publish:instance];
     [registrar addMethodCallDelegate:instance channel:channel];
-
-    SetGlobalOption("MDK_KEY",
-        "C03BFF5306AB39058A767105F82697F42A00FE970FB0E641D306DEFF3F220547E5E5377A3C504DC30D547890E71059BC023A4DD91");
-}
-@end
+    SetGlobalOption("MDK_KEY", "C03BFF5306AB39058A767105F82697F42A00FE970FB0E641D306DEFF3F220547E5E5377A3C504DC30D547890E71059BC023A4DD91
