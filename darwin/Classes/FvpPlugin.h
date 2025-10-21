a4562192ca666ca8c973c9f96bbf0b4a71ccb367
ssdaviss79 <daviss79@yahoo.com>
2025-10-21 12:46:34 +0800

    Update FvpPlugin.h

--- darwin/Classes/FvpPlugin.h
+++ darwin/Classes/FvpPlugin.h
@@ -1,35 +1,10 @@
-// FvpPlugin.h
+#if __has_include(<Flutter/Flutter.h>)
 #import <Flutter/Flutter.h>
+#else
+#import <FlutterMacOS/FlutterMacOS.h>
+#endif
 #import <AVFoundation/AVFoundation.h>
-#import <AVKit/AVKit.h>
 
-// Forward declarations
-@protocol FlutterTextureRegistry;
-@class FvpPipController;
-
-// Main plugin interface
-@interface FvpPlugin : NSObject<FlutterPlugin>
-
-// Flutter texture registry for rendering
-@property (nonatomic, strong, readonly) NSObject<FlutterTextureRegistry> *texRegistry;
-
-// Method channel for communication with Dart
-@property (nonatomic, strong) FlutterMethodChannel *channel;
-
-// Map of textureId -> PiP controllers
-@property (nonatomic, strong) NSMutableDictionary<NSNumber*, FvpPipController*> *pipControllers;
-
-// Designated initializer
-- (instancetype)initWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar;
-
-@end
-
-// PiP Controller interface (public)
-@interface FvpPipController : NSObject <AVPictureInPictureControllerDelegate>
-
-// Public properties
-@property (nonatomic, strong) AVPictureInPictureController *pipController;
-@property (nonatomic, assign) int64_t textureId;
-@property (nonatomic, strong) FlutterMethodChannel *channel;
-
+@interface FvpPlugin : NSObject<FlutterPlugin, AVPictureInPictureControllerDelegate>
+@property(readonly, strong, nonatomic) FlutterMethodChannel* channel;
 @end
