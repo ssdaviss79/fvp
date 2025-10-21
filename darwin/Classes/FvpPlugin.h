3e3e23af1dba4a0269c00e2aaa3d0b426f1f1962
ssdaviss79 <daviss79@yahoo.com>
2025-10-21 12:10:06 +0800

    Update FvpPlugin.h

--- darwin/Classes/FvpPlugin.h
+++ darwin/Classes/FvpPlugin.h
@@ -9,18 +9,27 @@
 
 // Main plugin interface
 @interface FvpPlugin : NSObject<FlutterPlugin>
+
+// Flutter texture registry for rendering
 @property (nonatomic, strong, readonly) NSObject<FlutterTextureRegistry> *texRegistry;
+
+// Method channel for communication with Dart
 @property (nonatomic, strong) FlutterMethodChannel *channel;
+
+// Map of textureId -> PiP controllers
 @property (nonatomic, strong) NSMutableDictionary<NSNumber*, FvpPipController*> *pipControllers;
 
 // Designated initializer
 - (instancetype)initWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar;
+
 @end
 
-// PiP Controller interface
+// PiP Controller interface (public)
 @interface FvpPipController : NSObject <AVPictureInPictureControllerDelegate>
+
+// Public properties
 @property (nonatomic, strong) AVPictureInPictureController *pipController;
-@property (nonatomic, strong) AVSampleBufferDisplayLayer *pipLayer;  // Updated: Sample buffer for FFmpeg
 @property (nonatomic, assign) int64_t textureId;
 @property (nonatomic, strong) FlutterMethodChannel *channel;
+
 @end
