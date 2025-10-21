6b1432d710e67bd40bf7aaa2da59cdaf5fb886c1
ssdaviss79 <daviss79@yahoo.com>
2025-10-21 11:46:16 +0800

    Update FvpPlugin.h

--- darwin/Classes/FvpPlugin.h
+++ darwin/Classes/FvpPlugin.h
@@ -3,31 +3,24 @@
 #import <AVFoundation/AVFoundation.h>
 #import <AVKit/AVKit.h>
 
-// Forward declarations for Flutter and custom classes
+// Forward declarations
 @protocol FlutterTextureRegistry;
 @class FvpPipController;
 
 // Main plugin interface
 @interface FvpPlugin : NSObject<FlutterPlugin>
-
-// Properties used in implementation
 @property (nonatomic, strong, readonly) NSObject<FlutterTextureRegistry> *texRegistry;
-@property (nonatomic, strong, readwrite) FlutterMethodChannel *channel;
-@property (nonatomic, strong, readwrite) NSMutableDictionary<NSNumber*, FvpPipController*> *pipControllers;
+@property (nonatomic, strong) FlutterMethodChannel *channel;
+@property (nonatomic, strong) NSMutableDictionary<NSNumber*, FvpPipController*> *pipControllers;
 
 // Designated initializer
 - (instancetype)initWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar;
-
-// PiP methods (private; declared for internal use)
-- (BOOL)enablePipForTexture:(int64_t)texId;
-- (BOOL)enterPipModeForTexture:(int64_t)texId width:(int)width height:(int)height;
-
 @end
 
 // PiP Controller interface
 @interface FvpPipController : NSObject <AVPictureInPictureControllerDelegate>
 @property (nonatomic, strong) AVPictureInPictureController *pipController;
-@property (nonatomic, strong) AVPlayerLayer *pipLayer;
+@property (nonatomic, strong) AVSampleBufferDisplayLayer *pipLayer;  // Updated: Sample buffer for FFmpeg
 @property (nonatomic, assign) int64_t textureId;
 @property (nonatomic, strong) FlutterMethodChannel *channel;
 @end
