45704a2502828d71bbcbae98d5cd9b9f2c484233
ssdaviss79 <daviss79@yahoo.com>
2025-10-21 10:25:25 +0800

    Update FvpPlugin.h

--- darwin/Classes/FvpPlugin.h
+++ darwin/Classes/FvpPlugin.h
@@ -3,17 +3,19 @@
 #import <AVFoundation/AVFoundation.h>
 #import <AVKit/AVKit.h>
 
+// Forward declaration
 @class FvpPipController;
 
 @interface FvpPlugin : NSObject<FlutterPlugin>
 - (instancetype)initWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar;
-- (BOOL)enablePipForTexture:(int64_t)texId;  // DECLARED METHODS
+- (BOOL)enablePipForTexture:(int64_t)texId;
 - (BOOL)enterPipModeForTexture:(int64_t)texId width:(int)width height:(int)height;
 @end
 
-// PiP Controller
-@interface FvpPipController : NSObject
+// PiP Controller interface
+@interface FvpPipController : NSObject <AVPictureInPictureControllerDelegate>
 @property (nonatomic, strong) AVPictureInPictureController *pipController;
-@property (nonatomic, strong) AVSampleBufferDisplayLayer *pipLayer;
+@property (nonatomic, strong) AVPlayerLayer *pipLayer;
 @property (nonatomic, assign) int64_t textureId;
+@property (nonatomic, strong) FlutterMethodChannel *channel;
 @end
