55d66d033df5395a2409e20ab30d6c4ff8a5d415
ssdaviss79 <daviss79@yahoo.com>
2025-10-21 09:35:58 +0800

    Update FvpPlugin.h

--- darwin/Classes/FvpPlugin.h
+++ darwin/Classes/FvpPlugin.h
@@ -1,7 +1,19 @@
-#if __has_include(<Flutter/Flutter.h>)
+// FvpPlugin.h
 #import <Flutter/Flutter.h>
-#else
-#import <FlutterMacOS/FlutterMacOS.h>
-#endif
+#import <AVFoundation/AVFoundation.h>
+#import <AVKit/AVKit.h>
+
+@class FvpPipController;
+
 @interface FvpPlugin : NSObject<FlutterPlugin>
+- (instancetype)initWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar;
+- (BOOL)enablePipForTexture:(int64_t)texId;  // DECLARED METHODS
+- (BOOL)enterPipModeForTexture:(int64_t)texId width:(int)width height:(int)height;
+@end
+
+// PiP Controller
+@interface FvpPipController : NSObject
+@property (nonatomic, strong) AVPictureInPictureController *pipController;
+@property (nonatomic, strong) AVSampleBufferDisplayLayer *pipLayer;
+@property (nonatomic, assign) int64_t textureId;
 @end
