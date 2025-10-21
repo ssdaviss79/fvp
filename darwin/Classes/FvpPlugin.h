a72f9272bb964849d7f31f2e58adcd2130b2d621
ssdaviss79 <daviss79@yahoo.com>
2025-10-21 15:28:28 +0800

    Update FvpPlugin.h

--- darwin/Classes/FvpPlugin.h
+++ darwin/Classes/FvpPlugin.h
@@ -4,15 +4,9 @@
 #import <FlutterMacOS/FlutterMacOS.h>
 #endif
 #import <AVFoundation/AVFoundation.h>
-#import <CoreMedia/CoreMedia.h>
-#if TARGET_OS_IPHONE
 #import <AVKit/AVKit.h>
-#endif
+#import <CoreMedia/CoreMedia.h>
 
-#if TARGET_OS_IPHONE
 @interface FvpPlugin : NSObject<FlutterPlugin, AVPictureInPictureControllerDelegate>
-#else
-@interface FvpPlugin : NSObject<FlutterPlugin>
-#endif
 - (void)syncFrameToPipForTextureId:(int64_t)textureId pixelBuffer:(CVPixelBufferRef)pixelBuffer;
 @end
